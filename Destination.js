3fff90d1bd3a717ab7190eae4aa3c05399dc68a2
Santhosh Krishnan M <santhoshkrishnanm2005@gmail.com>
2025-10-04 05:09:41 +0530

    Updated some files

--- Destination.js
+++ Destination.js
@@ -1,9 +1,8 @@
-// models/Destination.js
 const mongoose = require('mongoose');
 
 const destinationSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  type: { type: String }, // e.g., Hill Station, Beach
+  type: { type: String }, 
   rating: { type: Number },
   description: { type: String },
   imageUrl: { type: String }
--- admin.js
+++ admin.js
@@ -4,7 +4,7 @@ const adminSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   email:    { type: String, required: true, unique: true },
   phone:    { type: String, required: true },
-  //address:  { type: String, required: true },
+  address:  { type: String, required: true },
   password: { type: String, required: true }
 });
 
