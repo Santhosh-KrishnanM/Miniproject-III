06b85c004f2951e3ef209642daba0cd1f3511183
Santhosh Krishnan M <139557720+Santhosh-KrishnanM@users.noreply.github.com>
2025-10-31 00:17:43 +0530

    test

--- app.js
+++ app.js
@@ -338,7 +338,7 @@ app.delete('/destinations/:id', async (req, res) => {
 
 
 
-const Travel = require('../Travel');
+const Travel = require('./Travel');
 
 // ✅ Get all travels
 router.get('/travels', async (req, res) => {
